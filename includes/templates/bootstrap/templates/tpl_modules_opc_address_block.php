b6197bddf6cc81567ae6996248ebcbaadd617978
lat9 <cindy@vinosdefrutastropicales.com>
2022-04-27 07:49:07 -0400

    #8: Correcting parameter count for `zen_prepare_country_zones_pull_down`.

--- includes/templates/bootstrap/templates/tpl_modules_opc_address_block.php
+++ includes/templates/bootstrap/templates/tpl_modules_opc_address_block.php
@@ -1,12 +1,12 @@
 <?php
 // -----
 // Part of the One-Page Checkout plugin, provided under GPL 2.0 license by lat9 (cindy@vinosdefrutastropicales.com).
-// Copyright (C) 2017-2020, Vinos de Frutas Tropicales.  All rights reserved.
+// Copyright (C) 2017-2022, Vinos de Frutas Tropicales.  All rights reserved.
 //
 // This module is included by tpl_modules_opc_billing_address.php and tpl_modules_opc_shipping_address.php and
 // provides a common-formatting for those two address-blocks.
 //
-// Modified for use by the 'bootstrap' template:  Bootstrap/OPC v1.0.0
+// Modified for use by the 'bootstrap' template:  Bootstrap/OPC v1.0.4
 //
 ?>
 <!--bof address block -->
@@ -14,7 +14,7 @@
 // -----
 // Sanitize module input values.
 //
-if (!isset($opc_address_type) || !in_array($opc_address_type, array('bill', 'ship'))) {
+if (!isset($opc_address_type) || !in_array($opc_address_type, ['bill', 'ship'])) {
     trigger_error("Unknown value for opc_address_type ($opc_address_type).", E_USER_ERROR);
     exit();
 }
@@ -91,14 +91,13 @@ if (!$opc_disable_address_change) {
     }
 }
 
-
-if (ACCOUNT_GENDER == 'true') {
+if (ACCOUNT_GENDER === 'true') {
     $field_name = "gender[$which]";
     $male_id = "gender-male-$which";
     $female_id = "gender-female-$which";
-    echo '<span class="custom-control custom-radio custom-control-inline">' . zen_draw_radio_field ($field_name, 'm', ($address['gender'] == 'm'), "id=\"$male_id\"") . 
+    echo '<span class="custom-control custom-radio custom-control-inline">' . zen_draw_radio_field ($field_name, 'm', ($address['gender'] === 'm'), "id=\"$male_id\"") . 
     "<label class=\"custom-control-label radioButtonLabel\" for=\"$male_id\">" . MALE . '</label></span><span class="custom-control custom-radio custom-control-inline">' . 
-    zen_draw_radio_field ($field_name, 'f', ($address['gender'] == 'f'), "id=\"$female_id\"") . 
+    zen_draw_radio_field ($field_name, 'f', ($address['gender'] === 'f'), "id=\"$female_id\"") . 
     "<label class=\"custom-control-label radioButtonLabel\" for=\"$female_id\">" . FEMALE . '</label></span>' . 
     (zen_not_null(ENTRY_GENDER_TEXT) ? '<span class="alert">' . ENTRY_GENDER_TEXT . '</span>': ''); 
 
@@ -106,7 +105,7 @@ if (ACCOUNT_GENDER == 'true') {
 }
 
 echo $_SESSION['opc']->formatAddressElement($which, 'firstname', $address['firstname'], ENTRY_FIRST_NAME, TABLE_CUSTOMERS, 'customers_firstname', ENTRY_FIRST_NAME_MIN_LENGTH, ENTRY_FIRST_NAME_TEXT) . $clear_both;
-    
+
 echo $_SESSION['opc']->formatAddressElement($which, 'lastname', $address['lastname'], ENTRY_LAST_NAME, TABLE_CUSTOMERS, 'customers_lastname', ENTRY_LAST_NAME_MIN_LENGTH, ENTRY_LAST_NAME_TEXT) . $clear_both;
 
 $field_name = "zone_country_id[$which]";
@@ -116,14 +115,14 @@ $field_id = "country-$which";
       <?php echo zen_get_country_list($field_name, $address['country'], "id=\"$field_id\"") . 
       (zen_not_null(ENTRY_COUNTRY_TEXT) ? '<span class="alert">' . ENTRY_COUNTRY_TEXT . '</span>' : '') . $clear_both; ?>
 <?php
-if (ACCOUNT_STATE == 'true') {
+if (ACCOUNT_STATE === 'true') {
     $state_zone_id = "stateZone-$which";
     $zone_field_name = "zone_id[$which]";
 ?>
       <label class="inputLabel"><?php echo ENTRY_STATE; ?></label>
 <?php    
     if ($address['show_pulldown_states']) {
-        echo zen_draw_pull_down_menu($zone_field_name, zen_prepare_country_zones_pull_down($address['country'], $address['zone_id']), $address['zone_id'], "id=\"$state_zone_id\"");
+        echo zen_draw_pull_down_menu($zone_field_name, zen_prepare_country_zones_pull_down($address['country']), $address['zone_id'], "id=\"$state_zone_id\"");
         if (zen_not_null(ENTRY_STATE_TEXT)) {
             echo '<span class="alert">' . ENTRY_STATE_TEXT . '</span>';
         }
@@ -134,13 +133,13 @@ if (ACCOUNT_STATE == 'true') {
     echo $_SESSION['opc']->formatAddressElement($which, 'state', $address['state'], '', TABLE_ADDRESS_BOOK, 'entry_state', ENTRY_STATE_MIN_LENGTH, ENTRY_STATE_TEXT) . $clear_both;
 }
 
-if (ACCOUNT_COMPANY == 'true') {
+if (ACCOUNT_COMPANY === 'true') {
     echo $_SESSION['opc']->formatAddressElement($which, 'company', $address['company'], ENTRY_COMPANY, TABLE_ADDRESS_BOOK, 'entry_company', ENTRY_COMPANY_MIN_LENGTH, ENTRY_COMPANY_TEXT) . $clear_both;
 }
 
 echo $_SESSION['opc']->formatAddressElement($which, 'street_address', $address['street_address'], ENTRY_STREET_ADDRESS, TABLE_ADDRESS_BOOK, 'entry_street_address', ENTRY_STREET_ADDRESS_MIN_LENGTH, ENTRY_STREET_ADDRESS_TEXT) . $clear_both;
 
-if (ACCOUNT_SUBURB == 'true') {
+if (ACCOUNT_SUBURB === 'true') {
     echo $_SESSION['opc']->formatAddressElement($which, 'suburb', $address['suburb'], ENTRY_SUBURB, TABLE_ADDRESS_BOOK, 'entry_suburb', 0, ENTRY_SUBURB_TEXT) . $clear_both;
 }
 
